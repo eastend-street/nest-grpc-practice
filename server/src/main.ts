46c3e78bee37ebd5cc82c257bf2df8232aebf2ab
eastend-street <suminari.yamada@gmail.com>
2023-05-31 22:34:45 +0900

    feat: install grpc libraries and microservices

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'server',
+        protoPath: join(__dirname, 'server/sever.proto'),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
